39f8eace9126d29caa19a7dbc1eacc93fdf0d520
Glitch (glitch-hello-website) <none>
2022-03-14 13:34:04 +0000

    🏣🐑 Checkpoint ./css/style.css:42969091/414 ./js/script.js:42969091/304

--- js/script.js
+++ js/script.js
@@ -5,3 +5,13 @@ var typed = new Typed(".typing", {
   BackSpeed:60,
   loop:true
 });
+
+// Aside
+
+const nav = document.querySelector(".nav"),
+      navList = nav.querySelectorAll("li"),
+      totalNavList = navList.length;
+
+for(let i=0; i<totalNavList; i++) {
+  console.log(navList[i]);
+}
